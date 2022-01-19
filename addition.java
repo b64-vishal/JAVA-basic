9c4d046ba96d842d302649e3b6403be2870c4fe9
b64-vishal <80021235+b64-vishal@users.noreply.github.com>
2022-01-19 21:40:43 +0530

    Create addition.java
    this add two number by default input

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String args[]){
+        int a,b,c;
+        a=10;
+        b=2;
+        c=a+b;
+        System.out.println("Addition of two number :"+c);
+    }
+}
